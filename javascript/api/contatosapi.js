d8baa8af57420d41ec19f7bdf3817e255e330f0e
Ikaris <log4304.016@gmail.com>
2023-04-13 13:41:31 -0300

    Atualizando Link da Api

--- javascript/api/contatosapi.js
+++ javascript/api/contatosapi.js
@@ -1,7 +1,7 @@
 'use strict'
 
 export const pegarListaDeCursosApi = async () => {
-    const url = `http://localhost:8080/v1/lion-school/cursos`
+    const url = `https://jolly-fox-jacket.cyclic.app/v1/lion-school/cursos`
     const response = await fetch(url)
     const data = await response.json()
     const array = []
